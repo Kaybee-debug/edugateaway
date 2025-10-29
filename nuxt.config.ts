e28aeec20c26536e11361652a2ee44f1c78c7763
KaraboMalehuMolepo <molepok@mogaleintergrated.co.za>
2025-10-29 19:55:16 +0200

    Enable SSR and fix empty page issue - site now fully functional

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxtjs/tailwindcss'],
-  ssr: false,
+  ssr: true,
   app: {
     baseURL: '/edugateaway/',
     buildAssetsDir: '/_nuxt/'
